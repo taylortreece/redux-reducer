68593873bd42e350e54608548745d57881ee8fc9
taylortreece <tayloratreece@gmail.com>
2021-09-04 19:52:25 -0500

    commit tree f9dbc68e7d9354cbb15c6b4aaff75b605290a759

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action){
+    switch(state, action) {
+        case "ADD_FRIEND":
+          return { ...state, friends: [...state.friends, action.friend] };
+        case "REMOVE_FRIEND":
+          return state.friends.filter(friend => (friend.id !== action.friend.id));
+        default:
+          return state
+    }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,18 @@
+let state = {
+    numberOfPresents: 0
+}
+
+let action = {
+    type: "INCREASE"
+}
+
 export function managePresents(state, action){
+    switch(action.type) {
+        case "INCREASE":
+          return { numberOfPresents: state.numberOfPresents + 1};
+        case "DECREASE":
+          return { numberOfPresents: state.numberOfPresents - 1};
+        default:
+          return state;
+    } 
 }
